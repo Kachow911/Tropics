ff7c0e458a29da3fd7202da249a7d506e28283a8
Trivaxy <itsmeomar2003@yahoo.com>
2017-09-11 18:14:29 +0300

    Added Grapes and Apple, fixed Mango

--- Items/Apple.cs
+++ Items/Apple.cs
@@ -6,35 +6,33 @@ using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
 
-
-
 namespace Tropics.Items
 {
-    public class MagnificentMango : ModItem
+    public class Apple : ModItem
     {
 		public override void SetStaticDefaults()
 		{
-			DisplayName.SetDefault("Mango");
-			Tooltip.SetDefault("Grants you Well Fed for 30 seconds\nHeals 30 HP");
+			DisplayName.SetDefault("Apple");
+			Tooltip.SetDefault("Grants you swiftness for 1 minute\nHeals 60 HP");
 		}
 
         public override void SetDefaults()
         {
-            item.width = 36;
-            item.height = 36;
+            item.width = 24;
+            item.height = 26;
             item.maxStack = 999;
             item.rare = 5;
             item.useAnimation = 20;
             item.useTime = 20;
             item.useStyle = 1;
-            item.UseSound = SoundID.Item2;
+			item.UseSound = SoundID.Item2;
             item.consumable = true;
         }
 
         public override bool UseItem(Player player)
         {
-		   player.HealEffect(30);
-		   player.AddBuff(BuffID.WellFed, 3600 / 2);
+		   player.HealEffect(60);
+		   player.AddBuff(BuffID.Swiftness, 3600);
 		   return true;
         }
     }
--- Items/Grapes.cs
+++ Items/Grapes.cs
@@ -10,31 +10,31 @@ using Terraria.ModLoader;
 
 namespace Tropics.Items
 {
-    public class MagnificentMango : ModItem
+    public class Grapes : ModItem
     {
 		public override void SetStaticDefaults()
 		{
-			DisplayName.SetDefault("Mango");
-			Tooltip.SetDefault("Grants you Well Fed for 30 seconds\nHeals 30 HP");
+			DisplayName.SetDefault("Grapes");
+			Tooltip.SetDefault("Increases your max amount of minions by 1 for 3 minutes\nHeals 20 HP");
 		}
 
         public override void SetDefaults()
         {
-            item.width = 36;
-            item.height = 36;
+            item.width = 24;
+            item.height = 30;
             item.maxStack = 999;
             item.rare = 5;
             item.useAnimation = 20;
             item.useTime = 20;
             item.useStyle = 1;
-            item.UseSound = SoundID.Item2;
+			item.UseSound = SoundID.Item2;
             item.consumable = true;
         }
 
         public override bool UseItem(Player player)
         {
-		   player.HealEffect(30);
-		   player.AddBuff(BuffID.WellFed, 3600 / 2);
+		   player.HealEffect(20);
+		   player.AddBuff(BuffID.Summoning, 3600 * 3);
 		   return true;
         }
     }
